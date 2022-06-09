219ad72c9b99d4bda252cdb854225dad3d4d3fe9
sarbajit-sam <61025878+sarbajit-sam@users.noreply.github.com>
2022-06-09 15:25:34 -0600

    Started C based DSE

---
+++ SSETesting.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<gsl_matrix.h>
+#include<gsl_statistics.h>
+#include<gsl/gsl_comple.h>
+
+float Vs = 480/1.732;
+
+P = (10e3 + 5e3im)/3;
+
+float f = 60.0;
+float nw = 2*pi*60, R = 7.3728, L = 9.7785e-3;
+
+//Fault case
+float R = 0.005 + 0.01;  # Rf + Rg
+float L = 0;
+
+Is = conj(P/V)
+Z = Vs/Is
+
+
+
